ec217dc718bd98e351657110519e97ab2c1acfc3
Matheus Cruz <bromatheuscruz@gmail.com>
2019-07-27 01:23:47 -0300

    Spring Initializr

---
+++ src/main/java/com/iocruz/bookscode/BookscodeApplication.java
@@ -0,0 +1,13 @@
+package com.iocruz.bookscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookscodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookscodeApplication.class, args);
+	}
+
+}
